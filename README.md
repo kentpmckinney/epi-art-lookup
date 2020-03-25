b472a12e8e7606cea4749b01dfb6c7128d8fbe07
Kent McKinney <kent.p.mckinney@gmail.com>
2020-03-25 10:18:51 -0700

    add test for Met object

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ https://github.com/kentpmckinney/epicodus-art-lookup
 * 3/24/2020 - Troubleshot BetterDoctor API to no avail (1.5 hours)
 * 3/24/2020 - Researched an alternative API to use and settled on the Metropolitan Museum of Art API (0.5 hours)
 * 3/25/2020 - Implemented code to get data from the API (1.5 hours)
-
+* 3/25/2020 - Created a test (0.5 hours)
 
 ### Specifications:
 
--- __tests__/test.js
+++ __tests__/test.js
@@ -1,12 +1,25 @@
 
 
-describe('', () => {
+describe('Jest', () => {
 
 	beforeEach(() => {
 	});
 
 	test('Jest works properly', () => {
 		expect(0).toEqual(0);
+	});
+
+});
+
+describe('Met object', () => {
+
+	beforeEach(() => {
+		let met = Met();
+	});
+
+	test('Met object pulls data successfully', () => {
+		met.search('rembrandt','1600','1700');
+		expect(met.items.length).toEqual(36);
 	});
 	
 });
